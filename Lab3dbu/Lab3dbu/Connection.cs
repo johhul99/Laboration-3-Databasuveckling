1bcc5edc9c090c6cbfbe4f075e50cc086a200dd7
johhul99 <johan.hultin@iths.se>
2024-11-26 17:29:50 +0100

    Connection klar

---
+++ Lab3dbu/Lab3dbu/Connection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
+using MongoDB.Driver;
+
+
+namespace Lab3dbu
+{
+    public class Connection
+    {
+        public string ConnectionUri()
+        {
+            IConfigurationRoot config = new ConfigurationBuilder().AddUserSecrets<Connection>().Build();
+
+            string connectionUri_pt1 = "mongodb+srv://";
+            string un = config["username"];
+            string pw = config["password"];
+            string connectionUri_pt2 = "@cluster0.l2ff3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+            return connectionUri_pt1 + un + ":" + pw + connectionUri_pt2;
+        }
+    }
+}
---
+++ Lab3dbu/Lab3dbu/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using Lab3dbu;
+
+Connection con = new Connection();
+var settings = MongoClientSettings.FromConnectionString(con.ConnectionUri());
+settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+var client = new MongoClient(settings);
+var database = client.GetDatabase("lab3dbu");
+var users = database.GetCollection<BsonDocument>("products");
+
+var filter = Builders<BsonDocument>.Filter.Empty;
+var documents = await users.Find(filter).ToListAsync();
+
+foreach (var document in documents)
+{
+    Console.WriteLine(document);
+}
